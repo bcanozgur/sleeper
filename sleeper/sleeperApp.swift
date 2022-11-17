8457741c793dd65c9a94b0020c5c57354259b905
berkecanozgur <berkecanozgur@galatasaray.net>
2022-11-17 22:30:23 +0300

    Initial Commit

---
+++ sleeper/sleeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleeperApp.swift
+//  sleeper
+//
+//  Created by Berkecan Özgür on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct sleeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
