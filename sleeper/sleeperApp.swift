27a49d33a5413b728fe6c6aa0e5884018820c1c0
berkecanozgur <berkecanozgur@galatasaray.net>
2022-12-07 19:42:10 +0300

    - menubar feature initialized.

--- sleeper/sleeperApp.swift
+++ sleeper/sleeperApp.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 @main
-struct sleeperApp: App {
+struct swiftui_menu_barApp: App {
+    @State var currentNumber: String = "1"
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            Button("One") {
+                currentNumber = "1"
+            }
+                    .keyboardShortcut("1")
+            Button("Two") {
+                currentNumber = "2"
+            }
+                    .keyboardShortcut("2")
+            Button("Three") {
+                currentNumber = "3"
+            }
+                    .keyboardShortcut("3")
+            Divider()
+
+            Button("Quit") {
+
+                NSApplication.shared.terminate(nil)
+
+            }.keyboardShortcut("q")
+
         }
     }
 }
